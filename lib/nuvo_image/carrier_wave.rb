d826fe9505276d8c2f33b8ffdb90a3b459a8eb34
whonz <whonzmail@gmail.com>
2016-06-27 21:57:47 +0900

    rubocop, typo

--- lib/nuvo_image/carrier_wave.rb
+++ lib/nuvo_image/carrier_wave.rb
@@ -3,51 +3,50 @@ require 'active_support/concern'
 module NuvoImage
   module CarrierWave
     extend ActiveSupport::Concern
-  end
+    module ClassMethods
+      def read(to, auto_orient=true)
+        process read: [to, auto_orient]
+      end
 
-  module ClassMethods
-    def read(to, auto_orient=true)
-      process read: [to, auto_orient]
+      def crop(from, to, width, height, gravity=:Center)
+        process crop: [from, to, width, height, gravity]
+      end
+
+      def resize(from, to, width, height, interpolation=:area)
+        process resize: [from, to, width, height, interpolation]
+      end
+
+      def jpeg(from, quality=:high)
+        process jpeg: [from, quality]
+      end
     end
 
-    def crop(from, to, width, height, gravity=:Center)
-      process crop: [from, to, auto_orient]
+    attr_reader :nuvo_image, :nuvo_images
+
+    def cache!(new_file)
+      NuvoImage.process do |nuvo_image|
+        @nuvo_images = {}
+        @nuvo_image = nuvo_image
+        super
+        @nuvo_image = nil
+        @nuvo_images = {}
+      end
     end
 
-    def resize(from, to, width, height, interpolation=:area)
-      process resize: [from, to, width, height, interpolation]
+    def read(to, auto_orient)
+      nuvo_images[to] = nuvo_image.read(file.path, auto_orient)
     end
 
-    def jpeg(from, quality=:high)
-      process jpeg: [from, quality]
+    def crop(from, to, width, height, gravity)
+      nuvo_images[to] = nuvo_image.crop(nuvo_images[from], width, height, gravity)
     end
-  end
 
-  attr_reader :nuvo_image, :nuvo_images
-
-  def cache!(new_file)
-    NuvoImage.process do |nuvo_image|
-      @nuvo_images = {}
-      @nuvo_image = nuvo_image
-      super
-      @nuvo_image = nil
-      @nuvo_images = {}
+    def resize(from, to, width, height, interpolation)
+      nuvo_images[to] = nuvo_image.resize(nuvo_images[from], width, height, interpolation)
     end
-  end
 
-  def read(from, to, auto_orient)
-    nuvo_images[to] = nuvo_image.read(file.path, auto_orient)
-  end
-
-  def crop(from, to, width, height, gravity)
-    nuvo_images[to] = nuvo_image.crop(nuvo_images[from], width, height, gravity)
-  end
-
-  def resize(from, to, width, height, interpolation)
-    nuvo_images[to] = nuvo_image.resize(nuvo_images[from], width, height, interpolation)
-  end
-
-  def jpeg(from, quality)
-    nuvo_image.jpeg(nuvo_images[from], file.path, quality)
+    def jpeg(from, quality)
+      nuvo_image.jpeg(nuvo_images[from], file.path, quality)
+    end
   end
 end
