a563787163b6e2aa06b4ee5d127666737d9d4635
whonz <whonzmail@gmail.com>
2016-06-27 17:34:58 +0900

    edit enum

--- ext/nuvo_image/include/opencv.h
+++ ext/nuvo_image/include/opencv.h
@@ -5,13 +5,12 @@
 #ifndef NUVO_IMAGE_OPENCV_H
 #define NUVO_IMAGE_OPENCV_H
 
-#if OPENCV_VERSION_MAJOR == 2
 #include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+
+#if OPENCV_VERSION_MAJOR == 2
+#include <opencv2/highgui/highgui.hpp>
 #elif OPENCV_VERSION_MAJOR == 3
-#include <opencv2/core.hpp>
-#include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
 #endif
 
